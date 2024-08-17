79404a359e029cbce5920e234afc93c0d1436b0c
Petr Kurbatov <157517103+Xinumy@users.noreply.github.com>
2024-08-17 18:15:16 +0300

    UI Improve

--- app/src/main/java/com/kurbatov/coingeckoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/kurbatov/coingeckoapp/presentation/CoinDetailActivity.kt
@@ -29,34 +29,54 @@ class CoinDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCoinDetailBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-        if (!intent.hasExtra(EXTRA_ID)) {
+        if (!intent.hasExtra(EXTRA_ID) && intent.hasExtra(EXTRA_NAME)) {
             finish()
             return
         }
+
+        binding?.toolbarTitle?.text = intent.getStringExtra(EXTRA_NAME)
         val currentId = intent.getStringExtra(EXTRA_ID) ?: ""
         viewModel = ViewModelProvider(this)[CoinDetailViewModel::class.java]
         viewModel.loadDetailData(currentId)
         viewModel.getIsLoading().observe(this) { isLoading ->
-            binding?.progressBarLoadingDetail?.visibility = if (isLoading == true) View.VISIBLE else View.GONE
+            binding?.progressBarLoadingDetail?.visibility =
+                if (isLoading == true) View.VISIBLE else View.GONE
         }
         binding?.toolbarButton?.setOnClickListener {
             finish()
         }
+
+        viewModel.getIsLoadingFail().observe(this) { isLoadingFail ->
+            binding?.linearLayoutDetailConnectionFailed?.visibility =
+                if (isLoadingFail == true) View.VISIBLE else View.GONE
+            binding?.textViewDescriptionTitle?.visibility =
+                if (isLoadingFail == false) View.VISIBLE else View.GONE
+            binding?.textViewCategoriesTitle?.visibility =
+                if (isLoadingFail == false) View.VISIBLE else View.GONE
+        }
+
+        binding?.buttonReconnectDetail?.setOnClickListener {
+            viewModel.loadDetailData(currentId)
+        }
+
         viewModel.getCoinDetailInfo().observe(this, Observer {
-            binding?.toolbarTitle?.text = it.name
+
             binding?.textViewDescription?.text = it.description?.en?.replace(Regex("<.*?>"), "")
             binding?.textViewCategories?.text = it.categories.toString().removeSurrounding("[", "]")
             Picasso.get().load(it.image?.large).into(binding?.imageViewDetailLogo)
             Log.d(myLog, "Success in activity: $it")
         })
+
     }
 
     companion object {
         private var EXTRA_ID = "ID"
+        private var EXTRA_NAME = ""
 
-        fun newIntent(context: Context, id: String): Intent {
+        fun newIntent(context: Context, id: String, name: String): Intent {
             val intent = Intent(context, CoinDetailActivity::class.java)
             intent.putExtra(EXTRA_ID, id)
+            intent.putExtra(EXTRA_NAME, name)
             return intent
         }
 
